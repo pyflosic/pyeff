6d20b3e80cc2ae71d47856b5fbafde0c420c108c
Sebastian Schwalbe <48414071+theonov13@users.noreply.github.com>
2019-04-15 15:02:44 +0200

    Add files via upload

---
+++ tests/ethane/run_optimization.py
@@ -0,0 +1,16 @@
+from pyeff_optimizer import * 
+
+# structure 
+p_cfg = './structs/ethane.cfg'
+# variables 
+emax=0.0000001
+fmax=0.005
+steps = 10000
+fix_nuc = None
+# optimization 
+calc = pyeffBFGS(p_cfg,emax,fmax,steps,fix_nuc)
+calc.run()
+# show final structure 
+calc.view()
+# write final structure as xyz file 
+calc.write_xyz()
