e88816eed2af4dcdcc7f8877d27d9fa084e4ea58
theonov13 <theonov13@gmail.com>
2019-04-16 08:51:43 +0200

    adjust wrong file structure

--- tests/developer/eff_code/opt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# path to the pyeff src code 
-export ASE_EFF_PBC_COMMAND="/home/schwalbe/Programms/eff_distro_devil/eff/eff eff.cfg"
-export PYTHONPATH=../../../../../src:$PYTHONPATH
-python eff_optimizer_pbc.py > eff.log 
--- tests/developer/opt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# path to the pyeff src code 
-export PYTHONPATH=../../../../src:$PYTHONPATH
-python pyeff_optimizer_pbc.py > pyeff.log 
